e04368f42a31ba3460e0f8a8f89b8096d153bbfa
shaonq <shaonq@qq.com>
2021-07-07 11:12:48 +0800

    update vue code

--- src/views/routeFile.js
+++ src/views/routeFile.js
@@ -1,4 +1,4 @@
-let docs = ["环境初始化", "vue vue-route vuex"];
+let docs = ["环境初始化", "vue vue-route vuex","docker部署vue项目"];
 // let servers = ["HTTP接口文档"];
 let developer = ["基础依赖", "富文本", "甘特图", "日程表", "网页截图", "阅读pdf", "导出xlsx", "echarts5.0"];
 
